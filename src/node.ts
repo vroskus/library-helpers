b99a06bf89bf2f1b97c7ea1b224e4782e9e206fa
Vilius Roškus <vro@danskebank.lt>
2023-04-11 16:16:35 +0300

    Update

--- src/node.ts
+++ src/node.ts
@@ -3,6 +3,6 @@ import crypto from 'crypto';
 
 export * from './common';
 
-type $GenerateHash = (value: string) => string;
-
-export const generateHash: $GenerateHash = (v) => crypto.createHash('md5').update(v).digest('hex');
+export const generateHash = (
+  value: string,
+): string => crypto.createHash('md5').update(value).digest('hex');
